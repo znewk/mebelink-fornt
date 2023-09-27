afa571443d15acce411465d7e0f1296bcfbe7e68
znewk <zane.css34@gmail.com>
2023-09-27 11:00:35 +0600

    micro fix links

--- src/PhoneButton/index.js
+++ src/PhoneButton/index.js
@@ -4,7 +4,7 @@ import {Image} from "react-bootstrap";
 const PhoneButton = () => {
 
     return (
-        <a className={styles.container} target={'_blank'} href={'tel:+77068171765'}> 
+        <a className={styles.container} href={'tel:+77068171765'}> 
             <Image height={32} width={32} src={'/phone-call.png'}/>
         </a>
     )
